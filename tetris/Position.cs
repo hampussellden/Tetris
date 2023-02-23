ecfad5a266df3bab44f628af45fa256afb061c5c
hampussellden <hampussellden1@gmail.com>
2023-02-23 17:04:18 +0100

    Add Position class

---
+++ tetris/Position.cs
@@ -0,0 +1,13 @@
+namespace tetris;
+
+public class Position
+{
+    public int Row { get; set; }
+    public int Column { get; set; }
+
+    public Position(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+}
